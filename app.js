438e69ce4527fc4af3ac4aed29485944ef879db9
George Benjamin <msdcconnect@gmail.com>
2018-11-14 21:22:01 +0100

    Created handler for the home page

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({extended:false}))
 //Setting the view engine to handle bars
 app.engine("handlebars",exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 
+app.get("/", (req, res) =>{
+    res.render("home");
+});
+
+
 app.use((req, res) =>{
     //res.send("<h1>Hello world</h1>")
-    res.render("home");
 });
 
 http.createServer(app).listen(3000);
